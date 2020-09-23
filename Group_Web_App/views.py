267e3a1b410d56c75524f6a50a16c95ba7d026a1
kunaldewangan <dkunal3333@gmail.com>
2020-09-23 19:35:27 +0530

    project view file

---
+++ Group_Web_App/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class MainPage(LoginRequiredMixin,TemplateView):
+    template_name = 'main.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
