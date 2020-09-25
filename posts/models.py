1ab3b0a4344969dcb6f0b66c513067ee2672501c
kunaldewangan <dkunal3333@gmail.com>
2020-09-25 11:14:57 +0530

    updates on posts app model file

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+from groups.models import Group
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User,related_name='post_user',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.CharField(max_length=255)
+    group = models.ForeignKey(Group,related_name='post_group',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
+ 
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('posts:all_post')
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    
+
