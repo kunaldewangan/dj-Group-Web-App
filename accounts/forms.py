cbddfcc19bd1581eea95f4dc025b31a7b2fe98af
kunaldewangan <dkunal3333@gmail.com>
2020-11-23 23:19:30 +0530

    profile form update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from . import models
 
 class UserCreateForm(UserCreationForm):
     
@@ -11,3 +13,15 @@ class UserCreateForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].label = 'Display Name'
         self.fields['email'].label = 'Email Address'
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = models.Profile
+        fields = ('gender','date_of_birth','phone_no','city')
+    
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['gender'].label = 'Gender'
+        self.fields['date_of_birth'].label = 'Date Of Birth'
+        self.fields['date_of_birth'].help_text = 'Format is YYYY-MM-DD'
