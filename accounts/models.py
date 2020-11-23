e1f25710a10fa1c97cdbcb4934bb728806382bd9
kunaldewangan <dkunal3333@gmail.com>
2020-11-23 23:17:32 +0530

    django-signals update

--- accounts/models.py
+++ accounts/models.py
@@ -25,3 +25,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+# django-signal
+@receiver(post_save, sender=get_user_model())
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
