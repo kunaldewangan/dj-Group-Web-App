f545bdf8ae94f9346622439771eb89ae93b804f7
kunaldewangan <dkunal3333@gmail.com>
2020-11-25 23:35:05 +0530

    profile-picture and image name updates

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,8 @@ from django.contrib import auth
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
+import os
+from uuid import uuid4
 
 # Create your models here.
 
@@ -10,6 +12,16 @@ class User(auth.models.User,auth.models.PermissionsMixin):
     
     def __str__(self):
         return "@{}".format(self.username)
+
+def user_directory_path(instance, filename): 
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{0}.{1}'.format(instance.user.username, ext)
+    else:
+        # set filename as random string
+        filename = '{0}.{1}'.format(uuid4().hex, ext)
+    return 'profile_picture/{0}'.format(filename) 
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(),on_delete=models.CASCADE,related_name='profile')
@@ -22,6 +34,7 @@ class Profile(models.Model):
     date_of_birth = models.DateField(blank=True,null=True)
     phone_no = models.CharField(max_length=12,default='')
     city = models.CharField(max_length=250,default='')
+    profile_picture = models.ImageField(upload_to=user_directory_path,default='profile_picture/default_profile.png',blank=True)
 
     def __str__(self):
         return self.user.username
