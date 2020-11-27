b48f73d74f17111fb69c174a508b9ebc67c168d5
kunaldewangan <dkunal3333@gmail.com>
2020-11-27 18:20:33 +0530

    Post model Admin-site update

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from . import models
+from django.contrib.admin import ModelAdmin
 # Register your models here.
 
-admin.site.register(models.Post)
+class PostAdmin(ModelAdmin):
+    list_display = ['user','message','group','created_at']
+    list_filter = ['group__private','created_at']
+    search_fields = ['user__username','user__first_name','user__last_name','user__email','message','group__name','created_at']
+    readonly_fields = ['created_at']
+
+admin.site.register(models.Post,PostAdmin)
